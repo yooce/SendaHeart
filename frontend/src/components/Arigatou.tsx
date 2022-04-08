c5f85d2173ec6fdec878a17aee2d9ff0208fa08f
yooce <yooce.k@gmail.com>
2022-04-08 14:37:55 +0900

    送金準備

--- frontend/src/components/Arigatou.tsx
+++ frontend/src/components/Arigatou.tsx
@@ -1,21 +1,28 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ArigatouContext, CurrentAddressContext } from "./../hardhat/SymfoniContext";
-import { Navbar, Container, Button, Table } from 'react-bootstrap';
+import { Navbar, Container, Button, Table, Modal, Form } from 'react-bootstrap';
 import {BigNumber} from "ethers";
 import { TransactionResponse } from "@ethersproject/abstract-provider";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { use } from "chai";
 
 interface Props {}
+
+interface UserContext {
+  name: string,
+  address: string
+}
 
 export const Arigatou: React.FC<Props> = () => {
   const arigatou = useContext(ArigatouContext);
   const [currentAddress] = useContext(CurrentAddressContext);
-
+  const [show] = useState<boolean>(false);
   const [participated, setParticipated] = useState<boolean>(false);
   const [tokenAmount, setTokenAmount] = useState<BigNumber>(BigNumber.from(0));
   const [message, setMessage] = useState("");
   const [inputGreeting, setInputGreeting] = useState("");
+  const [users, setUsers] = useState<UserContext[]>();
   useEffect(() => {
     const doAsync = async () => {
       if (!arigatou.instance) return;
@@ -27,7 +34,12 @@ export const Arigatou: React.FC<Props> = () => {
 
         let users : { 0: string[], 1: string[] };
         users = await arigatou.instance.getUsers();
-        setMessage(users[0][0]);
+        let c_users : UserContext[] = new Array(users[0].length);
+        for (let i = 0; i < users[0].length; i++ ) {
+          c_users[i] = { name: users[0][i], address: users[1][i] };
+        }
+        setUsers(c_users);
+        setMessage(c_users[0].name + ' : ' + c_users[0].address);
       }
     };
     doAsync();
@@ -59,6 +71,14 @@ export const Arigatou: React.FC<Props> = () => {
     arigatou.instance.withdraw()
   }
 
+  const onClick = (addr: string) => {
+    console.log(addr);
+  }
+
+  const handleClose = () => {
+
+  }
+
   return (
     <div className="container">
       <Navbar bg="dark" variant="dark" fixed="top">
@@ -78,35 +98,58 @@ export const Arigatou: React.FC<Props> = () => {
       <Table striped bordered hover>
           <thead>
             <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>ユーザー名</th>
+              <th>アドレス</th>
+              <th>送付</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
+            {users?.map((user, index) => {
+              if (user.address != currentAddress) {
+                return (
+                  <tr key={index} v-for="user in users">
+                    <td>{user.name}</td>
+                    <td>{user.address}</td>
+                    <td><Button onClick={() => onClick(user.address)}>送付</Button></td>
+                  </tr>
+                )
+              }
+            })}
           </tbody>
         </Table>
-        <p>{message}</p>
-        <Button variant="primary">プライマリーボタン</Button>
       </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
