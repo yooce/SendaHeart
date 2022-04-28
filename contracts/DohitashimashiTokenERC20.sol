40079a76e30856a1cd09dd8314a374a5210fbe74
yooce <yooce.k@gmail.com>
2022-04-28 15:06:31 +0900

    DIT

---
+++ contracts/DohitashimashiTokenERC20.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./IMintable.sol";
+
+contract DohitashimashiTokenERC20 is ERC20 {
+    uint constant initialSupplies = 100000000;
+
+    address admin;
+
+    constructor() ERC20("ArigaToken", "ARGT") {
+        admin = msg.sender;
+        _mint(msg.sender, initialSupplies);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 0;
+    }
+}
